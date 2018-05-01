36465c576ed1c2881623a94556a3250ee221f774
xFuture603 <daniel.xfuture@googlemail.com>
2018-05-01 22:41:04 +0200

    fixed few contact form mistakes
    .

--- contactform.php
+++ contactform.php
@@ -3,13 +3,13 @@
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
+    $mailFrom = $_POST['email'];
     $message = $_POST['message'];
 
     $mailTo = "pingpongdesign@goawaybost.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
-    mail($mailTo, $subject, $txt, $headers);
+    mail($mailTo, $txt, $headers);
     header("Location: index.php?mailsend");
 }
